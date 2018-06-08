0993438442716c8aa3dffe505bbda81531e6a3e9
bkell11 <bkeller11@msn.com>
2018-06-07 19:41:59 -0600

    firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+var config = {
+    apiKey: "AIzaSyCqEnzaaYtKA0jk5bRBoCxs0HolhFlOwFA",
+    authDomain: "ticket-price-grabber-6f86d.firebaseapp.com",
+    databaseURL: "https://ticket-price-grabber-6f86d.firebaseio.com",
+    projectId: "ticket-price-grabber-6f86d",
+    storageBucket: "",
+    messagingSenderId: "34076773600"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
 //onclick function
 $("#search").on("click", function () {
 
