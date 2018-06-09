a594639cacd231ce261497f14abe5197feff526d
jdlibbee <libbeelibbee@yahoo.com>
2018-06-09 11:34:15 -0600

    finished spotify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,13 @@ $("#search").on("click", function (event) {
     console.log(lookup);
     console.log(search);
     $("#artistSearch").attr("placeholder", "Search Artist Name").val("");
-    spotify();
+    getAuth();
 
 })
 
 
 //spotify function
-function spotify() {
+function getAuth() {
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -36,60 +36,44 @@ function spotify() {
         console.log(authKey);
         auth = authKey.access_token;
         console.log("auth = " + auth);
+        getData();
     });
-    var settings2 = {
+}
+// function spotify() {
+//     getData();
+// }
+function getData() {
+    var dataSettings = {
         "async": true,
         "crossDomain": true,
         "url": `https://api.spotify.com/v1/search?q=${lookup}&type=track&market=US&limit=5`,
         "method": "GET",
         "headers": {
-            "Authorization": `Bearer  BQA-OxoRrlIxXk6YdKJqBY2WwpzgD9tE7U3JPAuU0Ol40uED8-HglUgc5V39VCF5VbcKOuFA67T7ITH2w7M`,
+            "Authorization": `Bearer  ${auth}`,
             "Cache-Control": "no-cache",
             "Postman-Token": "5f5d2a21-2c25-4f8f-9310-fff567e5cdaa"
         }
     }
-
-    $.ajax(settings2).then(function (searchResults) {
+    console.log("hello2");
+    $.ajax(dataSettings).then(function (searchResults) {
         console.log(searchResults);
+
+        console.log(searchResults.tracks.items);
+
+        $('#artist').html(`<h5 class="card-title">${lookup}</h5>`);
+        $('#songs').html(`<ol class="card-text" id="button" type="1"></ol>`);
         for (var i = 0; i < searchResults.tracks.items.length; i++) {
-            // if (searchResults.error.status == 401) {
-            //     // var settings = {
-            //     //     "async": true,
-            //     //     "crossDomain": true,
-            //     //     "url": "https://accounts.spotify.com/api/token",
-            //     //     "method": "POST",
-            //     //     "headers": {
-            //     //         "Content-Type": "application/x-www-form-urlencoded",
-            //     //         "Authorization": "Basic N2ExYjBjZTdiMmI3NDcyNTgxNWQ3OTQ2ZTk3ZGM5MmE6MDUzYjJiZmJjMjg0NDliMWJlNDYzNjViMzEzYWZkZjM=",
-            //     //         "Cache-Control": "no-cache",
-            //     //         "Postman-Token": "1a6ba0e7-3b06-429a-afe8-cbbd11e5997d"
-            //     //     },
-            //     //     "data": {
-            //     //         "grant_type": "client_credentials"
-            //     //     }
-            //     // }
-
-            //     // $.ajax(settings).done(function (authKey) {
-            //     //     console.log(authKey);
-            //     //     var artist = search;
-            //     //     console.log(artist);
-            //     //     auth = authKey.access_token;
-            //     //     console.log("auth = " + auth);
-            //     // });
-            // } else {
-            // }
-            console.log(searchResults.tracks.items[i].name);
-            $('#artist').html(`<h5 class="card-title">${lookup}</h5>`);
-            $('#songs').html(`
-                <ol class="card-text" type="1">
-                    <li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[0].preview_url}" target="_blank">${searchResults.tracks.items[0].name}</a></buttion></li>
-                    <li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[1].preview_url}" target="_blank">${searchResults.tracks.items[1].name}</a></buttion></li>
-                    <li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[2].preview_url}" target="_blank">${searchResults.tracks.items[2].name}</a></buttion></li>
-                    <li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[3].preview_url}" target="_blank">${searchResults.tracks.items[3].name}</a></buttion></li>
-                    <li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[4].preview_url}" target="_blank">${searchResults.tracks.items[4].name}</a></buttion></li>
-                </ol>
-
-                    `);
+            $('#button').append(`<li><button class="btn btn-light ml-2"><a href="${searchResults.tracks.items[i].preview_url}" target="_blank">${searchResults.tracks.items[i].name}</a></buttion></li>`);
         }
-    });
-}
+    }).fail(async function (jqXHR, textStatus, errorThrown) {
+        alert("Request failed: " + textStatus);
+        console.log(errorThrown, jqXHR);
+        if (jqXHR.status == 401) {
+            console.log("hello");
+            await getAuth();
+            getData();
+        };
+    })
+
+};
+
